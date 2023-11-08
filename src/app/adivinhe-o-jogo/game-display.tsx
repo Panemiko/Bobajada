4045d24d5852332bdd9535ed9d3a2c3a49907a3b
Panemiko <titoluccao@gmail.com>
2023-11-08 11:29:33 +0000

    eu vou me

--- src/app/adivinhe-o-jogo/game-display.tsx
+++ src/app/adivinhe-o-jogo/game-display.tsx
@@ -23,6 +23,9 @@ export function GameDisplay() {
     }
 
     getGameList();
+  }, []);
+
+  useEffect(() => {
     refreshGame();
 
     document.addEventListener("keydown", (e) => {
@@ -30,8 +33,7 @@ export function GameDisplay() {
         refreshGame();
       }
     });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [refreshGame]);
 
   return (
     <>
