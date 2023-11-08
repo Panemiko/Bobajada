deaab26371a359c501483e008494cbeaf4d7d103
Panemiko <titoluccao@gmail.com>
2023-11-08 11:01:42 +0000

    a

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,7 @@ export function cn(...params: ClassValue[]) {
 
 export function getBaseUrl() {
   if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return process.env.VERCEL_URL; // SSR should use vercel url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
   return `http://localhost:${process.env.PORT}`; // dev SSR should use localhost
 }
