66703fed31c15cc92400dd972d7e48d3fc37fb4c
Tito Lucca Oliveira <titoluccao@gmail.com>
2024-03-06 22:03:55 -0300

    feat: suika game proxy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  "rewrites": [
+    {
+      "source": "/suika",
+      "destination": "https://suika.world/:path*"
+    },
+  ]
+};
 
 module.exports = nextConfig;
