b2be25de24efb0edce2d89cfba3f6785196c64f8
Tito Lucca Oliveira <titoluccao@gmail.com>
2025-04-23 20:15:46 -0300

    feat: add anime english names

--- src/app/api/anime-list/route.ts
+++ src/app/api/anime-list/route.ts
@@ -2,5 +2,10 @@ import { NextResponse } from "next/server";
 import animeList from "./data.json";
 
 export async function GET() {
-  return NextResponse.json(animeList, { status: 200 });
+  const formattedData = animeList.map((anime) => ({
+    name: anime.english || anime.name,
+    code: anime.code,
+  }));
+
+  return NextResponse.json(formattedData, { status: 200 });
 }
