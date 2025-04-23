42cb3a36ec6009ccced8869f3523f670c8e10978
Tito Lucca Oliveira <titoluccao@gmail.com>
2025-04-23 01:42:02 -0300

    feat: update gamedle api route

--- src/app/api/game-list/route.ts
+++ src/app/api/game-list/route.ts
@@ -1,31 +1,24 @@
-import { redis } from "@/lib/redis";
 import { NextResponse } from "next/server";
 
-export const runtime = "edge";
-
 export async function GET() {
-  let redisGameList = await redis.get<string>("game-list");
+  const res = await fetch("https://www.gamedle.wtf/newtokenizedversion", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Connection: "keep-alive",
+      Cookie: "JSESSIONID=1",
+    },
+  });
 
-  if (!redisGameList) {
-    const res = await fetch("https://gamedle.wtf/search2");
-
-    if (res.status !== 200) {
-      return new Response(undefined, { status: 500 });
-    }
-
-    const games = (await res.json()) as {
-      value: number;
-      label: string;
-      collection: number;
-    }[];
-
-    const mappedGames = games.map((game) => game.label);
-
-    // game-list to redis
-    redisGameList = (await redis.set("game-list", JSON.stringify(mappedGames), {
-      ex: 86400, // expires in 24 hours
-    })) as string;
+  if (res.status !== 200) {
+    return new Response(undefined, { status: 500 });
   }
 
-  return NextResponse.json(redisGameList, { status: 200 });
+  const games = (await res.json()).gameList as {
+    label: string;
+  }[];
+
+  const mappedGames = games.map((game) => game.label);
+
+  return NextResponse.json(mappedGames, { status: 200 });
 }
